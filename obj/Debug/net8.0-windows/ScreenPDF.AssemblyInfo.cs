91868c5ca582d6e7ca8e6fd23bcef4b2a963e99e
Dmitriy Andreev <dmtrii.a@gmail.com>
2026-01-17 06:20:09 +0300

    published the application to the folder

--- obj/Debug/net8.0-windows/ScreenPDF.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/ScreenPDF.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScreenPDF")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1953630438c7af87e57d7e2f8bd400d4bfcef7e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91bf7e461a34ac98514b097c69f3f1a36bd53e4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScreenPDF")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScreenPDF")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
